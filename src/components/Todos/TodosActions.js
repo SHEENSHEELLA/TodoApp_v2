4ae7fb60dd79f96e27e767f1c1ead61e710479bd
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-13 14:47:05 +0300

    Fix mistake in TodosActions

--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -5,7 +5,7 @@ import styles from './TodosActions.module.css'
 function TodosActions({ resetTodos, deleteCompletedTodos, completedTodosExist }) {
   return (
     <div className={styles.todosActionsContainer}>
-      <Button Button title="Reset Todos" onClick={resetTodos}><RiRefreshLine /></Button>
+      <Button title="Reset Todos" onClick={resetTodos}><RiRefreshLine /></Button>
       <Button title="Clear Completed Todos" onClick={deleteCompletedTodos} disabled={!completedTodosExist}><RiDeleteBin2Line /></Button>
     </div >
   )
